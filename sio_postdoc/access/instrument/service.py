2b6d3390c30d8eeed39c7d69d3d2d6ada2b5f489
Rich Inman <rinman24@gmail.com>
2024-02-27 22:19:53 -0800

    Adds BlobAccess Protocol

--- sio_postdoc/access/instrument/service.py
+++ sio_postdoc/access/instrument/service.py
@@ -1,11 +1,15 @@
+import dataclasses
+import os
 import re
 from calendar import monthrange
 from collections import defaultdict
 from datetime import date, datetime, time, timedelta
 from pathlib import Path
-from typing import Union
+from typing import Protocol, Union
 
 import netCDF4 as ncdf
+from azure.core.exceptions import HttpResponseError, ResourceExistsError
+from azure.storage.blob import BlobServiceClient
 
 from sio_postdoc.access.instrument.constants import DATADIR, MONTH_DIRECTORIES
 from sio_postdoc.access.instrument.contracts import (
@@ -19,6 +23,90 @@ from sio_postdoc.access.instrument.contracts import (
     RawTimeHeightData,
     TimeHeightData,
 )
+
+
+class BlobAccess(Protocol):
+    """Define protocol for Azure Blob Storage."""
+
+    # pylint: disable=missing-function-docstring
+
+    def create_container(self, name: str) -> str: ...
+    def add_blob(self, name: str, path: Path) -> None: ...
+    def list_blobs(self, name: str) -> tuple[str, ...]: ...
+
+
+@dataclasses.dataclass
+class Account:
+    """Azure Account Details."""
+
+    name: str
+    key: str
+
+
+@dataclasses.dataclass
+class Endpoint:
+    """Azure Endpoint Details."""
+
+    protocol: str
+    blob: str
+
+
+class InstrumentAccess(BlobAccess):
+    """Concrete implementation of Blob Access."""
+
+    def __init__(self) -> None:
+        self._account: Account = Account(
+            name=os.environ["STORAGE_ACCOUNT_NAME"],
+            key=os.environ["STORAGE_ACCOUNT_KEY"],
+        )
+        self._endpoint: Endpoint = Endpoint(
+            protocol=os.environ["DEFAULT_ENDPOINTS_PROTOCOL"],
+            blob=os.environ["BLOB_STORAGE_ENDPOINT"],
+        )
+
+        self._blob_service: BlobServiceClient = (
+            BlobServiceClient.from_connection_string(conn_str=self.connection_string)
+        )
+
+    @property
+    def connection_string(self) -> str:
+        """Return the Connection String."""
+        return (
+            f"AccountName={self._account.name};"
+            + f"AccountKey={self._account.key};"
+            + f"DefaultEndpointsProtocol={self._endpoint.protocol};"
+            + f"BlobEndpoint={self._endpoint.blob};"
+        )
+
+    @property
+    def blob_service(self) -> BlobServiceClient:
+        """Return instance of Azure BlobServiceClient."""
+        return self._blob_service
+
+    def create_container(self, name: str) -> None:
+        """Create a new blob container."""
+        message: str = "Success."
+        try:
+            with self.blob_service.get_container_client(name) as container_client:
+                container_client.create_container()
+        except ResourceExistsError:
+            message = "Container already exists."
+        except HttpResponseError:
+            message = "Container name contains invalid characters."
+        return message
+
+    def add_blob(self, name: str, path: Path) -> None:
+        """Add a blob to the given container."""
+        with self.blob_service.get_container_client(name) as container:
+            with open(path, "rb") as data:
+                container.upload_blob(name=path.name, data=data)
+
+    def list_blobs(self, name: str) -> tuple[str, ...]:
+        """List the contents of the container."""
+        blobs: tuple[str, ...]
+        with self.blob_service.get_container_client(name) as container:
+            blobs = tuple(sorted([str(blob.name) for blob in container.list_blobs()]))
+        return blobs
 
 
 def _identify_years(range: DateRange) -> list[int]:
---
+++ tests/access/instrument/integration/blob_access_protocol_test.py
@@ -0,0 +1,71 @@
+"""Test methods in BlobAccess Protocol."""
+
+import os
+from pathlib import Path
+from typing import Generator
+
+import pytest
+from dotenv import find_dotenv, load_dotenv
+
+from sio_postdoc.access.instrument.service import InstrumentAccess
+
+# pylint: disable=redefined-outer-name
+
+EMPTY_CONTAINTER: str = "emptycontainer"
+PRE_POPULATED_CONTAINER: str = "prepopulated"
+DATA_DIRECTORY: Path = Path(
+    os.getcwd() + "/tests/access/instrument/integration/test_blobs/"
+)
+FILE_NAMES: tuple[Path, ...] = (
+    "eureka.txt",
+    "sheba.txt",
+    "utqiagvik.txt",
+)
+
+
+@pytest.fixture(scope="module")
+def service() -> Generator[InstrumentAccess, None, None]:
+    """Module level service for testing."""
+    # Setup
+    load_dotenv(find_dotenv())
+    _service: InstrumentAccess = InstrumentAccess()
+    # Empty container
+    _service.create_container(EMPTY_CONTAINTER)
+    # Pre populated container
+    _service.create_container(PRE_POPULATED_CONTAINER)
+    for file_name in FILE_NAMES:
+        _service.add_blob(PRE_POPULATED_CONTAINER, DATA_DIRECTORY / file_name)
+    # Test
+    yield _service
+    # Teardown
+    for container in _service.blob_service.list_containers():
+        _service.blob_service.delete_container(container["name"])
+
+
+def test_create_non_existing_container(service):
+    """Test creation of a container that does not exist."""
+    assert service.create_container("notexist") == "Success."
+
+
+def test_create_existing_container(service):
+    """Test creation of a container that exists."""
+    assert service.create_container(EMPTY_CONTAINTER) == "Container already exists."
+
+
+def test_create_invalid_characters_container(service):
+    """Test creation of a container with invalid characters."""
+    assert (
+        service.create_container("Invalid")
+        == "Container name contains invalid characters."
+    )
+
+
+def test_list_blobs(service):
+    """Test that container contents are listed correctly."""
+    assert service.list_blobs(PRE_POPULATED_CONTAINER) == FILE_NAMES
+
+
+def test_add_blob(service):
+    """Test that files are successfully uploaded to blob storage."""
+    service.add_blob(EMPTY_CONTAINTER, DATA_DIRECTORY / FILE_NAMES[0])
+    assert service.list_blobs(EMPTY_CONTAINTER) == (FILE_NAMES[0],)
