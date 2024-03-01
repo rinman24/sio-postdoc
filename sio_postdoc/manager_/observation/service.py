e240a151dc6c9b65a556fb2080f1a840b0287753
Rich Inman <rinman24@gmail.com>
2024-02-29 19:31:34 -0800

    Adds format_dir method for the manager

---
+++ sio_postdoc/manager_/observation/service.py
@@ -0,0 +1,33 @@
+"""Observation Manager Module."""
+
+from pathlib import Path
+
+from sio_postdoc.access._local.service import LocalAccess
+from sio_postdoc.engine.formatting.service import FormattingContext
+from sio_postdoc.engine.formatting.strategies import AbstractDateStrategy, MMDDhhmm
+
+Content = tuple[Path, ...]
+
+
+class ObservationManager:
+
+    def __init__(self) -> None:
+        self._local_access: LocalAccess = LocalAccess()
+        self._strategy: AbstractDateStrategy = MMDDhhmm()
+        self._formatting_context: FormattingContext = FormattingContext(self._strategy)
+
+    @property
+    def local_access(self) -> LocalAccess:
+        return self._local_access
+
+    @property
+    def formatting_context(self) -> FormattingContext:
+        return self._formatting_context
+
+    def format_dir(self, directory: Path, suffix: str):
+        """Format the directory using the current formatting context."""
+        current: Content = self.local_access.list_files(directory, suffix)
+        new: Content = tuple(
+            file.parent / self.formatting_context.format(file.name) for file in current
+        )
+        self.local_access.rename_files(current, new)
---
+++ tests/manager_/observation/integration/batch_rename_test.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Generator
+
+import pytest
+
+from sio_postdoc.engine.formatting.strategies import YYYYMMDDdothhmmss
+from sio_postdoc.manager_.observation.service import ObservationManager
+
+Content = tuple[str, ...]
+
+
+@dataclass
+class _TestData:
+    raw: Content
+    formatted: Content
+
+
+TEMP_DIRECTORY: Path = Path(
+    os.getcwd() + "/tests/manager_/observation/integration/temp_files/"
+)
+
+SUFFIX: str = ".txt"
+
+TEST_DATA: _TestData = _TestData(
+    raw=(
+        TEMP_DIRECTORY / "mmcr.20240107.180455.txt",
+        TEMP_DIRECTORY / "mmcr.20240229.064020.txt",
+    ),
+    formatted=(
+        TEMP_DIRECTORY / "mmcr.D2024-01-07T18-04-55.txt",
+        TEMP_DIRECTORY / "mmcr.D2024-02-29T06-40-20.txt",
+    ),
+)
+
+
+@pytest.fixture(scope="module")
+def service() -> Generator[ObservationManager, None, None]:
+    """Module level service for testing."""
+    _service: ObservationManager = ObservationManager()
+    _service.formatting_context.strategy = YYYYMMDDdothhmmss()
+    # Setup
+    try:
+        os.mkdir(TEMP_DIRECTORY)
+    except FileExistsError:
+        pass
+    for path in TEST_DATA.raw:
+        with open(TEMP_DIRECTORY / path.name, mode="w", encoding="utf-8") as file:
+            file.write(f"Original name: {path.name}")
+    # Test
+    yield _service
+    # Teardown
+    shutil.rmtree(TEMP_DIRECTORY)
+
+
+def test_format_directory(service):
+    """Test that all files in a directory with a given extention are formatted."""
+    service.format_dir(TEMP_DIRECTORY, SUFFIX)
+    assert (
+        service.local_access.list_files(TEMP_DIRECTORY, SUFFIX) == TEST_DATA.formatted
+    )
