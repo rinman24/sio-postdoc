d8511e255317de0baccf7bbbd52fd24860433034
Rich Inman <rinman24@gmail.com>
2024-03-26 10:55:45 -0700

    Adds repr to InstrumentData

--- sio_postdoc/access/instrument/contracts.py
+++ sio_postdoc/access/instrument/contracts.py
@@ -48,3 +48,34 @@ class InstrumentData(BaseModel):
     name: str
     observatory: str
     notes: str
+
+    def __repr__(self) -> str:
+        first: bool
+
+        repr_: str = ""
+        repr_ += "<class 'sio_postdoc.access.instrument.contracts.InstrumentData'>\n"
+        repr_ += f"    instrument name: {self.name}\n"
+        repr_ += f"    observatory name: {self.observatory}\n"
+        repr_ += f"    notes: {self.notes}\n"
+        repr_ += f"    initial time: {self.time.initial}\n"
+        repr_ += f"    dimensions(sizes): time({len(self.time.offsets)})"
+        for dimension in self.axis:
+            repr_ += f", {dimension.name}({len(dimension.values)})"
+        if self.vectors:
+            repr_ += "\n    vectors(dimensions): "
+        first = True
+        for vector in self.vectors:
+            if not first:
+                repr_ += ", "
+            repr_ += f"{vector.name}(time)"
+            first = False
+        if self.matrices:
+            repr_ += "\n    matrices(dimensions): "
+        first = True
+        for matrix in self.matrices:
+            if not first:
+                repr_ += ", "
+            repr_ += f"{matrix.name}(time, {self.axis[0].name})"
+            first = False
+
+        return repr_
---
+++ tests/access/instrument/unit/contracts_representation_test.py
@@ -0,0 +1,99 @@
+"""Test repr of instrument access contracts."""
+
+from datetime import datetime
+
+from sio_postdoc.access.instrument.contracts import (
+    InstrumentData,
+    PhysicalMatrix,
+    PhysicalVector,
+    TemporalVector,
+)
+
+# pylint: disable=missing-function-docstring
+
+TIME: TemporalVector = TemporalVector(
+    initial=datetime.now(),
+    offsets=(10, 20, 30),
+    units="seconds",
+    name="time",
+    scale=1,
+    flag=-999,
+    dtype="f4",
+)
+
+AXIS: tuple[PhysicalVector, ...] = (
+    PhysicalVector(
+        values=(45, 90),
+        units="meters",
+        name="range",
+        scale=1,
+        flag=-999,
+        dtype="f4",
+    ),
+)
+
+PHYSICAL_VECTORS: tuple[PhysicalVector, ...] = (
+    PhysicalVector(
+        values=(32.88, 32.881, 32.8812),
+        units="degrees",
+        name="latitude",
+        scale=1,
+        flag=-999,
+        dtype="f4",
+    ),
+    PhysicalVector(
+        values=(117.23, 117.234, 117.2344),
+        units="degrees",
+        name="longitude",
+        scale=1,
+        flag=-999,
+        dtype="f4",
+    ),
+)
+
+PHYSICAL_MATRICES: tuple[PhysicalMatrix, ...] = (
+    PhysicalMatrix(
+        values=((-9, -24), (-1, -7), (-3, -20)),
+        units="dBZ",
+        name="reflectivity",
+        scale=1,
+        flag=-999,
+        dtype="f4",
+    ),
+    PhysicalMatrix(
+        values=((24, 84), (7, 89), (3, 87)),
+        units="unknown",
+        name="far_parallel",
+        scale=1,
+        flag=-999,
+        dtype="f4",
+    ),
+)
+
+
+def test_instrument_data_repr():
+    # Arrange
+    data = InstrumentData(
+        time=TIME,
+        axis=AXIS,
+        matrices=PHYSICAL_MATRICES,
+        vectors=PHYSICAL_VECTORS,
+        name="test_name",
+        observatory="test_observatory",
+        notes="test notes",
+    )
+    expected: tuple[str, ...] = (
+        "<class 'sio_postdoc.access.instrument.contracts.InstrumentData'>",
+        "    instrument name: test_name",
+        "    observatory name: test_observatory",
+        "    notes: test notes",
+        "    initial time: ",
+        "    dimensions(sizes): time(3), range(2)",
+        "    vectors(dimensions): latitude(time), longitude(time)",
+        "    matrices(dimensions): reflectivity(time, range), far_parallel(time, range)",
+    )
+    # Act
+    result: str = data.__repr__()
+    # Assert
+    for substring in expected:
+        assert substring in result
