51be322fc039c2cee21631482ec76c05e113e677
Rich Inman <rinman24@gmail.com>
2024-04-29 18:45:30 -0700

    Adds initial and padding to window

--- sio_postdoc/engine/transformation/window.py
+++ sio_postdoc/engine/transformation/window.py
@@ -2,11 +2,36 @@
 
 from math import ceil, floor
 
-from pydantic import BaseModel
 
-
-class GridWindow(BaseModel):
+class GridWindow:
     """Encapsulate a 2-dimensional window."""
+
+    def __init__(self, length: int, height: int) -> None:
+        """Initialize the `GridWindow`."""
+        self._length: int = length
+        self._height: int = height
+        _horizontal_padding: tuple[int, int] = self._get_padding(size=length)
+        _vertical_padding: tuple[int, int] = self._get_padding(size=height)
+        self._padding: dict[str, int] = {
+            "left": _horizontal_padding[0],
+            "right": _horizontal_padding[1],
+            "bottom": _vertical_padding[0],
+            "top": _vertical_padding[1],
+        }
+        self._initial: tuple[int, int] = (
+            self._padding["left"],
+            self._padding["bottom"],
+        )
+
+    @property
+    def initial(self) -> tuple[int, int]:
+        """Return the initial position of the `GridWindow`."""
+        return self._initial
+
+    @property
+    def padding(self) -> dict[str, int]:
+        """Return the padding for the `GridWindow`."""
+        return self._padding
 
     @staticmethod
     def _get_padding(size: int) -> tuple[int, int]:
--- tests/engine/transformation/unit/window_test.py
+++ tests/engine/transformation/unit/window_test.py
@@ -12,7 +12,7 @@ def size(request) -> int:
 
 
 def test_get_padding(size):
-    window: GridWindow = GridWindow()
+    window: GridWindow = GridWindow(length=size, height=size)
     expected: tuple[int, int]
     match size:
         case 1:
@@ -33,3 +33,91 @@ def test_get_padding(size):
             expected = (3, 4)
     result: int = window._get_padding(size)
     assert result == expected
+
+
+def test_horizontal_window(size):
+    window: GridWindow = GridWindow(length=size, height=1)
+    initial: tuple[int, int]
+    padding: dict[str, int] = {}
+    match size:
+        case 1:
+            initial = (0, 0)
+            padding["left"] = 0
+            padding["right"] = 0
+        case 2:
+            initial = (0, 0)
+            padding["left"] = 0
+            padding["right"] = 1
+        case 3:
+            initial = (1, 0)
+            padding["left"] = 1
+            padding["right"] = 1
+        case 4:
+            initial = (1, 0)
+            padding["left"] = 1
+            padding["right"] = 2
+        case 5:
+            initial = (2, 0)
+            padding["left"] = 2
+            padding["right"] = 2
+        case 6:
+            initial = (2, 0)
+            padding["left"] = 2
+            padding["right"] = 3
+        case 7:
+            initial = (3, 0)
+            padding["left"] = 3
+            padding["right"] = 3
+        case 8:
+            initial = (3, 0)
+            padding["left"] = 3
+            padding["right"] = 4
+    assert window.initial == initial
+    assert window.padding["left"] == padding["left"]
+    assert window.padding["right"] == padding["right"]
+    assert window.padding["bottom"] == 0
+    assert window.padding["top"] == 0
+
+
+def test_horizontal_window(size):
+    window: GridWindow = GridWindow(length=1, height=size)
+    initial: tuple[int, int]
+    padding: dict[str, int] = {}
+    match size:
+        case 1:
+            initial = (0, 0)
+            padding["bottom"] = 0
+            padding["top"] = 0
+        case 2:
+            initial = (0, 0)
+            padding["bottom"] = 0
+            padding["top"] = 1
+        case 3:
+            initial = (0, 1)
+            padding["bottom"] = 1
+            padding["top"] = 1
+        case 4:
+            initial = (0, 1)
+            padding["bottom"] = 1
+            padding["top"] = 2
+        case 5:
+            initial = (0, 2)
+            padding["bottom"] = 2
+            padding["top"] = 2
+        case 6:
+            initial = (0, 2)
+            padding["bottom"] = 2
+            padding["top"] = 3
+        case 7:
+            initial = (0, 3)
+            padding["bottom"] = 3
+            padding["top"] = 3
+        case 8:
+            initial = (0, 3)
+            padding["bottom"] = 3
+            padding["top"] = 4
+    assert window.initial == initial
+    assert window.padding["left"] == 0
+    assert window.padding["right"] == 0
+    assert window.padding["bottom"] == padding["bottom"]
+    assert window.padding["top"] == padding["top"]
