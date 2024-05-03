3c13e493d4a1baa119656c5f536c21f9110324ca
Rich Inman <rinman24@gmail.com>
2024-05-03 02:00:01 -0700

    Adds masks strategy

---
+++ sio_postdoc/engine/transformation/strategies/masks.py
@@ -0,0 +1,98 @@
+"""Define strategy to extract masks from data."""
+
+from pathlib import Path
+
+import sio_postdoc.utility.service as utility
+from sio_postdoc.access import DataSet
+from sio_postdoc.engine import Dimensions, Scales, Units
+from sio_postdoc.engine.transformation.contracts import (
+    DateTime,
+    Dimension,
+    DType,
+    Variable,
+    VariableRequest,
+)
+from sio_postdoc.engine.transformation.strategies.base import TransformationStrategy
+
+NINES: int = -999
+
+
+class Masks(TransformationStrategy):
+    """Engine logic for masks from data."""
+
+    def _add_dimensions(self, dataset: DataSet, path: Path) -> None:
+        """Use a `DataSet` to set the state of `_dimensions`."""
+        self._dimensions["time"] = Dimension(
+            name=Dimensions.TIME,
+            size=dataset.dimensions["time"].size,
+        )
+        self._dimensions["level"] = Dimension(
+            name=Dimensions.LEVEL,
+            size=dataset.dimensions["level"].size,
+        )
+
+    def _add_variables(self, dataset: DataSet, path: Path) -> None:
+        """Use a `DataSet` to set the state of `_variables`."""
+        self._add_epoch(path)
+        self._add_offset(dataset)
+        self._add_range(dataset)
+        self._add_cloud_mask(dataset)
+
+    def _add_epoch(self, path: Path) -> None:
+        # Change this to base: it should not be epoch
+        extracted: DateTime = utility.extract_datetime(path.name, time=False)
+        value: int = DateTime(
+            year=extracted.year,
+            month=extracted.month,
+            day=extracted.day,
+            hour=0,
+            minute=0,
+            second=0,
+        ).unix
+        self._variables["epoch"] = Variable(
+            dtype=DType.I4,
+            long_name="Unix Epoch 1970 of Initial Timestamp",
+            scale=Scales.ONE,
+            units=Units.SECONDS,
+            dimensions=(),
+            values=value,
+        )
+
+    def _add_offset(self, dataset: DataSet) -> None:
+        value_request: VariableRequest = VariableRequest(
+            variable="offset",
+            name="offset",
+            long_name="Seconds Since Initial Timestamp",
+            units=Units.SECONDS,
+            scale=Scales.ONE,
+            dtype=DType.I4,
+            flag=NINES,
+            dimensions=(self._dimensions["time"],),
+        )
+        self._add_single_variable(dataset, value_request)
+
+    def _add_range(self, dataset: DataSet) -> None:
+        value_request: VariableRequest = VariableRequest(
+            variable="range",
+            name="range",
+            long_name="Return Range",
+            units=Units.METERS,
+            scale=Scales.ONE,
+            dtype=DType.U2,
+            flag=NINES,
+            dimensions=(self._dimensions["level"],),
+        )
+        self._add_single_variable(dataset, value_request)
+
+    def _add_cloud_mask(self, dataset: DataSet) -> None:
+        value_request: VariableRequest = VariableRequest(
+            variable="cloud_mask",
+            name="cloud_mask",
+            long_name="Cloud Mask",
+            units=Units.NONE,
+            scale=Scales.ONE,
+            dtype=DType.I4,
+            flag=NINES,
+            dimensions=(self._dimensions["time"], self._dimensions["level"]),
+        )
+        self._add_single_variable(dataset, value_request)
